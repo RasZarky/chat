1229ac08cee222fd98d3e4a72e0791e9a0e508f5
RasZarky <ubdoolrazak@gmail.com>
2024-03-25 15:30:10 +0000

    chatspage added

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,14 @@
-const ChatsPage = () => {
-    return <>chats ...</>
+import {MultiChatSocket, MultiChatWindow, useMultiChatLogic} from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+    const ChatProps = useMultiChatLogic('e7b78aff-742e-4772-8776-589b8882f67b',
+     props.user.username, 
+     props.user.secret)
+
+    return <div style={{ height: '100vh' }}>
+            <MultiChatSocket {...ChatProps}/>
+            <MultiChatWindow {...ChatProps} style={{ height: '100%' }}/>
+         </div>
 }
 
 export default ChatsPage
